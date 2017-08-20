41ebfcdebb943cff4e4bcbd1f34169993c5d9ad5
junsoo90 <asdfrf1@daum.net>
2017-08-20 18:06:05 +0900

    정렬

--- Algorithm/beakjoon/chapter9/Main_2751.java
+++ Algorithm/beakjoon/chapter9/Main_2751.java
@@ -11,7 +11,8 @@ public class Main_2751 {
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(buf.readLine());
-        ArrayList<Integer> list = new ArrayList();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
         for(int i=0; i<n; i++){
             list.add(Integer.parseInt(buf.readLine()));
         }
