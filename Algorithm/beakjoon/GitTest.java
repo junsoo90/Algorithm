8e1ee5154cd8a5d3c80a0d5ff9d319c11fd68df4
junsoo90 <asdfrf1@daum.net>
2017-08-20 19:29:17 +0900

    테스트7

--- Algorithm/beakjoon/GitTest.java
+++ Algorithm/beakjoon/GitTest.java
@@ -2,6 +2,6 @@ package Algorithm.beakjoon;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("테스트6");
+        System.out.println("테스트7");
     }
 }
