584986bfe4c6ab8c74da51167800272554013857
junsoo <junsoo@junsoo-PC>
2017-11-22 16:23:31 +0900

    Insert Sort

---
+++ Algorithm/Basic/Sort/InsertSort/InsertSort.java
@@ -0,0 +1,23 @@
+package InsertSort;
+
+public class InsertSort {
+	public static void main(String[] args) {
+
+		int arr[] = { 5, 4, 6, 2, 7, 1, 3 };
+		int i, j, temp;
+		for (i = 1; i < arr.length; i++) {
+			temp = arr[i];
+			for (j = i - 1; j >= 0; j--) {
+				if (arr[j] > temp)
+					arr[j + 1] = arr[j];
+				else {
+					break;
+				}
+			}
+			arr[j + 1] = temp;
+		}
+
+		for (int k = 0; k < arr.length; k++)
+			System.out.println(arr[k]);
+	}
+}
