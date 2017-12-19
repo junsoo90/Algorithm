96861a7e800fb9119795e354d7a61a2adca877f2
junsoo <junsoo@junsoo-PC>
2017-12-19 14:14:45 +0900

    MergeSort update

--- Algorithm/Basic/Sort/MergeSort/MergeSort.java
+++ Algorithm/Basic/Sort/MergeSort/MergeSort.java
@@ -24,22 +24,22 @@ public class MergeSort {
 	public static void merge(int arr[], int left, int mid, int right) {
 		int temp[] = new int[arr.length];
 		int l = left;
-		int r = mid;
+		int r = mid + 1;
 		int t = left;
 
-		while (l < mid && right > r) {
-			if (arr[l] < arr[r])
+		while (l <= mid && right >= r) {
+			if (arr[l] <= arr[r])
 				temp[t++] = arr[l++];
 			else
 				temp[t++] = arr[r++];
 		}
 
-		while (l < mid)
+		while (l <= mid)
 			temp[t++] = arr[l++];
-		while (r < right)
+		while (r <= right)
 			temp[t++] = arr[r++];
 
-		for (int i = left; i < right; i++)
+		for (int i = left; i <= right; i++)
 			arr[i] = temp[i];
 
 	}
