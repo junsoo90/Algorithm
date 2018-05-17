398aa547d6bd667a125552b6f7d039a3b6619440
junsoo90 <asdfrf1@daum.com>
2018-05-17 16:55:53 +0900

    분수찾기

--- Algorithm/beakjoon/chapter8/Main_1193.java
+++ Algorithm/beakjoon/chapter8/Main_1193.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int input = Integer.parseInt(in.readLine());
